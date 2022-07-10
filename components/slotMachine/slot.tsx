192c625e4c5e80cf1a67eb1ed9c34d79b2918884
AlvinShin <83438225+AlvinShin@users.noreply.github.com>
2022-07-09 20:44:54 -0400

    test logo for mobile

--- components/slotMachine/slot.tsx
+++ components/slotMachine/slot.tsx
@@ -16,72 +16,73 @@ export const Slot: React.FC<{
     index,
     size,
 }) => {
-    const srcs:string[] = [
-        "/slotIcons/apple.png",
-        "/slotIcons/bar.png",
-        "/slotIcons/bell.png",
-        "/slotIcons/cherry.png",
-        "/slotIcons/clover.png",
-        "/slotIcons/coin.png",
-        "/slotIcons/diamond.png",
-        "/slotIcons/grape1.png",
-        "/slotIcons/grape2.png",
-        "/slotIcons/heart.png",
-        "/slotIcons/horseshoe.png",
-        "/slotIcons/lemon.png",
-        "/slotIcons/orange.png",
-        "/slotIcons/watermelon.png"
-    ];
+        const srcs: string[] = [
+            "/slotIcons/apple.png",
+            "/slotIcons/bar.png",
+            "/slotIcons/bell.png",
+            "/slotIcons/cherry.png",
+            "/slotIcons/clover.png",
+            "/slotIcons/coin.png",
+            "/slotIcons/diamond.png",
+            "/slotIcons/grape1.png",
+            "/slotIcons/grape2.png",
+            "/slotIcons/heart.png",
+            "/slotIcons/horseshoe.png",
+            "/slotIcons/lemon.png",
+            "/slotIcons/orange.png",
+            "/slotIcons/watermelon.png"
+        ];
 
-    const randNumInit = getRand(srcs.length);
-    
-    const slot: React.ReactElement<any, any>[] = [
-        <Image src={srcs[randNumInit]} width={(size === "normal") ? 24 : 20.8} height={(size === "normal") ? 24 : 20.8} alt="#" key={1} />
-    ];
-    srcs.slice(randNumInit, 1);
+        const randNumInit = getRand(srcs.length);
+
+        const slot: React.ReactElement<any, any>[] = [
+            <Image src={srcs[randNumInit]} width={(size === "normal") ? 24 : 20.8} height={(size === "normal") ? 24 : 20.8} alt="#" key={1} />
+        ];
+        srcs.slice(randNumInit, 1);
 
 
-    for (var numSlots = 0; numSlots < index; numSlots++) {
-        const randNum = getRand(srcs.length);
-        slot.push(<Image src={srcs[randNum]} width={(size === "normal") ? 24 : 20.8} height={(size === "normal") ? 24 : 20.8} alt="#" key={numSlots + 2} />);
-        srcs.splice(randNum, 1);
-    }
+        for (var numSlots = 0; numSlots < index; numSlots++) {
+            const randNum = getRand(srcs.length);
+            slot.push(<Image src={srcs[randNum]} width={(size === "normal") ? 24 : 20.8} height={(size === "normal") ? 24 : 20.8} alt="#" key={numSlots + 2} />);
+            srcs.splice(randNum, 1);
+        }
 
-    return (
-        <div
-            key={index + 1}
-            style={
-                {
-                    "--trans-dist": `${-40 * (index + 1)}px`,
-                    "--trans-time": `${(index + 1) * 0.067}s`,
-                } as React.CSSProperties
-            }
-            className="translate-y-[var(--trans-dist)] animate-[scrollUp_var(--trans-time)_ease-in-out_1_1s_forwards]"
-        >
+        return (
             <div
-                key={1}
-                className={
-                    "text-[19px] w-[1.3rem] sm:w-[1.5rem] h-[2.5rem] bg-[#fff] flex justify-center items-center uppercase font-[500] "
-                    + ((index !== 0) ? "border-[#3b4e52] border-l-[2px] " : "rounded-l-[15px]")
-                    + ((index === textLen - 1) ? "rounded-r-[15px]" : "")
+                key={index + 1}
+                style={
+                    {
+                        "--trans-dist": `${-40 * (index + 1)}px`,
+                        "--trans-time": `${(index + 1) * 0.067}s`,
+                    } as React.CSSProperties
                 }
+                className="translate-y-[var(--trans-dist)] animate-[scrollUp_var(--trans-time)_ease-in-out_1_1s_forwards]"
             >
-                {letter}
-            </div>
-
-            {slot.map((e, index) => {
-                return (
-                    <div
-                        key={index + 2}
-                        className={"bg-[#fff] w-[1.3rem] sm:w-[1.5rem] h-[2.5rem] py-[8px] flex justify-center items-center "
-                        + ((index !== 0) ? "border-[#3b4e52] border-l-[2px] " : "")
+                <div
+                    key={1}
+                    className={
+                        "text-[19px] w-[1.3rem] sm:w-[1.5rem] h-[2.5rem] bg-[#fff] flex justify-center items-center uppercase font-[500] "
+                        + ((index !== 0) ? "border-[#3b4e52] border-l-[2px] " : "rounded-l-[15px]")
+                        + ((index === textLen - 1) ? "rounded-r-[15px]" : "")
                     }
-                    >
-                        {e}
-                    </div>
-                )
-            })}
+                >
+                    {letter}
+                </div>
 
-        </div>
-    );
-}
+                {slot.map((e, index) => {
+                    return (
+                        <div
+                            key={index + 2}
+                            className={"bg-[#fff] w-[1.3rem] sm:w-[1.5rem] h-[2.5rem] py-[8px] flex justify-center items-center "
+                                + ((index !== 0) ? "border-[#3b4e52] border-l-[2px] " : "rounded-l-[15px]")
+                                + ((index === textLen - 1) ? "rounded-r-[15px]" : "")
+                            }
+                        >
+                            {e}
+                        </div>
+                    )
+                })}
+
+            </div>
+        );
+    }
