49e9d43437cdec97d3acbe1aa35e17b3072c8040
AlvinShin <83438225+AlvinShin@users.noreply.github.com>
2022-07-14 22:45:11 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from "next";
 import React from "react";
 import Head from "next/head";
-import YouTube from "react-youtube";
 
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 import { Birthday } from "../components/birthday";
-
+import { YoutubeSection } from "../components/youtubeSection";
 import { Socials } from "../components/socials/socials";
 
 const Home: NextPage = () => {
@@ -15,20 +14,15 @@ const Home: NextPage = () => {
             <Head>
                 <title>moneysigneric</title>
                 <link rel="shortcut icon" href="/favicon.png" />
-                <meta name="description" content={"Hey guys, moneysigneric here (meow meow)\nWelcome to my website that contains all my socials!"} />
+                <meta name="description" content={"Hey guys, moneysigneric here!\nWelcome to my website which contains all of my socials!"} />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="author" content="Made by Alvin Shin" />
                 <meta name="og:image" content="/favicon.png" />
             </Head>
             <Header />
-            <div className="pt-[8rem] pb-[2rem] h-full w-full flex flex-col justify-center items-center bg-[#fff] dark:bg-[#333232] text-[#292929] dark:text-[#fff]">
+            <div className="pt-[8rem] pb-[2rem] h-full sm:h-screen w-full flex flex-col justify-center items-center bg-[#fff] dark:bg-[#333232] text-[#292929] dark:text-[#fff]">
                 <Birthday />
-                {/* <div>
-                    <h1 className="text-[25px] text-center mb-2">Check out my latest video!</h1>
-                    <YouTube
-                        videoId="pDIaXSTKuZU"
-                    />
-                </div> */}
+                <YoutubeSection />
                 <Socials />
             </div>
             <Footer />
